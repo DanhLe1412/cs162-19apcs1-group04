d888828548288235b709fc91390e102f02dcf7de
LouisDo2108 <louisdo2108@gmail.com>
2020-05-18 09:33:42 +0700

    Nothing

--- Class/Class/class.h
+++ Class/Class/class.h
@@ -12,7 +12,7 @@ struct User
 	string username;
 	string password;
 	int type = -1; //(default: -1)(1: staff, 2 : lecturer, 3 : student)
-	User* next;
+	User *next = nullptr;
 };
 
 struct Class
@@ -25,17 +25,17 @@ struct Student
 {
 	string lastname;
 	string firstname;
-	int ID;
+	string ID;
 	string DoB;
 	bool gender; // 0: Female, 1: Male
 	string list_course[20];
-	Class* student_class;
+	Class *student_class;
 };
 
 void input_student_info(Student *&student);
 void import_students_csv(ifstream &fi, ofstream &fo, string csv_name);
 void add_new_student(ifstream &fi, ofstream &fo, string class_name);
-void edit_student(ifstream& fi, ofstream& fo, string class_name, string student_id);
+void edit_student(ifstream &fi, ofstream &fo, string class_name, string student_id);
 string remove_student(ifstream &fi, ofstream &fo, string class_name, string student_id);
 void change_students(ifstream &fi, ofstream &fo, string class_name_A, string class_name_B, string student_id);
 void view_list_classes(ifstream &fi);
